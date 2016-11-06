f623964bfb3bac4612a44a9ba0e2cab8cc4c7030
searleb <billsearle@me.com>
2016-11-06 18:29:46 +1100

    shortened version

--- Week-2/Bill/flatten.js
+++ Week-2/Bill/flatten.js
@@ -1,16 +1,9 @@
-var array = [[0, 1], 2, 3, [4, 5, [6, [7], 8], [9]]]
-var newArray = [];
+var golf = [[0, 1], 2, 3, [4, 5, [6, [7], 8], [9]]]
 
-function loop(array) {
-   array.forEach( (a, i) => {
-      if (Array.isArray(a)) {
-         loop(a)
-      } else if(!Array.isArray()) {
-         newArray.push(a)
-      } else if (i === array.length) {
-         return;
-      }
-      console.log( newArray )
-   })
-}
-loop(array);
+// a = array
+// p = temp progress array
+// r = results array
+
+l=(a,p)=>{p?r=p:r=[];a.forEach((e)=>{Array.isArray(e)?l(e,r):r.push(e)});return r}
+
+console.log(l(golf));
