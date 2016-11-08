fd49a9517a15371e9b9d329dae84ed7cb851fc96
Sasha Yee <sasha.yee@gmail.com>
2016-11-08 15:16:02 +1100

    Added doctest to flatten.

--- Week-2/Sasha/flatten/lib/flatten.ex
+++ Week-2/Sasha/flatten/lib/flatten.ex
@@ -1,5 +1,18 @@
 defmodule Flatten do
 
+  @doc """
+  Flatten a nested list to a single level list.
+
+  ## Examples:
+  iex> Flatten.list [1, 2, 3, 4]
+  [1, 2, 3, 4]
+
+  iex> Flatten.list [1, 2, 3, [4, 5, [6, 7, 8]]]
+  [1, 2, 3, 4, 5, 6, 7, 8]
+
+  iex> Flatten.list ["sasha", ["yee", [30, ["parramatta"]]]]
+  ["sasha", "yee", 30, "parramatta"]
+  """
   def list([]) do
     []
   end
